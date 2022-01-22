d7393fe41ba37696ef9fcd54d307314b3b298be5
Elto Oliveira da Silva <petran.elto@gmail.com>
2022-01-22 01:03:28 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/chanllege/back/ChanllegeBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.chanllege.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanllegeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanllegeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/chanllege/back/ChanllegeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.chanllege.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanllegeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
